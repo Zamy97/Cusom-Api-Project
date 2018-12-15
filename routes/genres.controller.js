520d2a3e3bbcc000727307eaf17f367a21cc8457
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-14 16:54:29 -0800

    changed file names

--- routes/genres.controller.js
+++ routes/genres.controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Genre = require('../models/genre.model.js');
+const Genre = require('../models/genre.js');
 
 router.get('/api/genres', function(req, res, next) {
     Genre.getGenres(function(err, genres) {
